2e3a9b79e20447d5a066a150f2111040b3e55097
bix <bixmail6@gmail.com>
2020-09-17 16:06:45 -0700

    add user registration with tests

--- test/auth-endpoints.spec.js
+++ test/auth-endpoints.spec.js
@@ -66,7 +66,7 @@ describe("Auth Endpoints", function () {
         .expect(400, { error: `Incorrect user_name or password` });
     });
 
-    it.only(`responds 200 and JWT auth token using secret when valid credentials`, () => {
+    it(`responds 200 and JWT auth token using secret when valid credentials`, () => {
       const userValidCreds = {
         user_name: testUser.user_name,
         password: testUser.password,
